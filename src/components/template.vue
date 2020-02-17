13f6c3b5852eccd2ba54a6a8bd9269590b1b5b43
Aone1 <Aone1_zyl@163.com>
2020-02-17 22:40:54 +0800

    实现主页布局

---
+++ src/components/template.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    模板文件
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            //
+        }
+    },
+    created(){},
+    mounted(){},
+    methods:{
+        //
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    
+</style>
---
+++ src/components/user/users.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Users
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            //
+        }
+    },
+    created(){},
+    mounted(){},
+    methods:{
+        //
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    
+</style>
