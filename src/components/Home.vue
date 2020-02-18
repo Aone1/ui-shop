ae7e0fbf00416db31071bedb10aafb251cb8ad51
Aone1 <Aone1_zyl@163.com>
2020-02-18 23:05:56 +0800

    用户列表完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,7 +14,8 @@
       <el-aside :width="isCollapse?'64px':'200px'">
         <div class="toggle-button" @click="toggleCollapse">|||</div>
         <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF"
-            :unique-opened="true" :collapse="isCollapse" :collapse-transition="false" router>
+            :unique-opened="true" :collapse="isCollapse" :collapse-transition="false" router
+            :default-active="activePath">
           <!-- 一级菜单 -->
           <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
             <template slot="title">
@@ -22,7 +23,8 @@
               <span>{{item.authName}}</span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item :index="'/'+subItem.path" v-for="subItem in item.children" :key="subItem.id">
+            <el-menu-item :index="'/'+subItem.path" v-for="subItem in item.children" 
+                :key="subItem.id" @click="saveNavState('/'+subItem.path)">
               <template slot="title">
                 <i class="el-icon-menu"></i>
                 <span>{{subItem.authName}}</span>
@@ -52,11 +54,13 @@ export default {
           '102':'iconfont icon-danju',
           '145':'iconfont icon-baobiao'
       },
-      isCollapse:false
+      isCollapse:false,
+      activePath:''
     };
   },
   created(){
       this.getMenuList();
+      this.activePath=window.sessionStorage.getItem('activePath');
   },
   methods: {
     logout() {
@@ -74,6 +78,11 @@ export default {
     },
     toggleCollapse(){
         this.isCollapse=!this.isCollapse;
+    },
+    saveNavState(activePath){
+        //保存链接的激活状态
+        window.sessionStorage.setItem('activePath',activePath);
+        this.activePath=activePath;
     }
   }
 };
--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -1,24 +1,106 @@
 <template>
   <div>
-    Users
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入搜索内容" v-model="queryInfo.query" clearable @clear="getUserList">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+
+      <!-- 用户列表区域 -->
+      <el-table :data="list" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column prop="username" label="姓名"></el-table-column>
+        <el-table-column prop="email" label="邮箱"></el-table-column>
+        <el-table-column prop="mobile" label="电话"></el-table-column>
+        <el-table-column prop="role_name" label="角色"></el-table-column>
+        <el-table-column prop="role_name" label="状态">
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row)"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="190px">
+          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+          </el-tooltip>
+        </el-table-column>
+      </el-table>
+
+      <!-- 分页区域 -->
+      <el-pagination
+        layout="total, sizes, prev, pager, next, jumper"
+        :page-sizes="[1, 2, 5, 10]"
+        :total="total"
+        :current-page="this.queryInfo.pagenum"
+        :page-size="this.queryInfo.pagesize"
+        @size-change="changePageSize"
+        @current-change="changeCurrentPage"
+      ></el-pagination>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            //
-        }
+  data() {
+    return {
+      list: [],
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      },
+      total: 0
+    };
+  },
+  created() {
+    this.getUserList();
+  },
+  mounted() {},
+  methods: {
+    getUserList() {
+      this.requestQuickGet(
+        `/manager/users?query=${this.queryInfo.query}&pagenum=${this.queryInfo.pagenum}&pagesize=${this.queryInfo.pagesize}`
+      ).then(resp => {
+        this.list = resp.data.data.users;
+        this.total = resp.data.data.total;
+      });
     },
-    created(){},
-    mounted(){},
-    methods:{
-        //
+    changePageSize(size) {
+      this.queryInfo.pagesize = size;
+      this.getUserList();
+    },
+    changeCurrentPage(page) {
+      this.queryInfo.pagenum = page;
+      this.getUserList();
+    },
+    userStateChanged(userinfo){
+        this.requestPut(`/manager/users/${userinfo.id}/state/${userinfo.mg_state}`).then(resp=>{
+            if(resp.data.meta.status!=200){
+                userinfo.mg_state=!userinfo.mg_state;
+                return this.$message.error('更新用户状态失败！');
+            }
+            this.$message.success("更新用户状态成功！");
+        })
     }
-}
+  }
+};
 </script>
 
 <style lang="less" scoped>
-    
 </style>
